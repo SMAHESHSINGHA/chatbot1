44c9aca4d6c881f42c040d6a0accc932f0128107
S Mahesh Singha <maheshsingha24@gmail.com>
2023-01-06 00:04:22 +0530

    Initial commit

---
+++ response.js
@@ -0,0 +1,16 @@
+const responseObj = {
+    hello: "Hey ! How can i help you ?",
+    hey: "Hey! What's Up",
+    "tell me my name":"S Mahesh Singha",
+    course:"MCA First year",
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+    "what is your name": "Vision",
+    "how are you":"I am fine thank you",
+    "what your favourite song":"Ed Sheeren - Perfect",
+    "whats your favourite colour": "Red and Black",
+    "whats your lucky number":"18",
+    "can you solve a rubix cube":"yes",
+
+
+};
